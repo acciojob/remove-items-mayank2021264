e69aabe3fc2a1839c693a3ae18f20d719b5d6bc4
MAYANK PANDEY <101644584+mayank2021264@users.noreply.github.com>
2026-04-14 13:20:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let button = document.getElementById('btn');
+
+function removeColor(event){
+	let select = document.getElementById('colorSelect');
+	let color = select.value;
+	console.log(color);
+	select.remove(select.selectedIndex);
+	console.log("removed",color);
+}
+
+button.addEventListener('click', removeColor);
